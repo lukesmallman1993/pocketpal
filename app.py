2b2b1d075948f047bd60305f6a3bae7814e311a7
LukeSmallman <lukesmallman15@gmail.com>
2021-12-14 21:42:26 +0000

    Add search functionality

--- app.py
+++ app.py
@@ -24,88 +24,166 @@ def get_recipes():
     recipes = list(mongo.db.recipes.find())
     return render_template("recipes.html", recipes=recipes)
 
+
+# search functionality
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    recipes = list(mongo.db.recipes.find({"$text": {"$search": query}}))
+    return render_template("recipes.html", recipes=recipes)
+
+
+# User registration
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        # check if username already exists in db
+        # check if username already exists
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
         if existing_user:
             flash("Username already exists")
             return redirect(url_for("register"))
-
+        # if username does not already exist
         register = {
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password"))
-        }
+            }
         mongo.db.users.insert_one(register)
-
-        # put the new user into 'session' cookie
+        # put new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
+
+# User login
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        # check if username exists in db
+        # check if username already exists
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
         if existing_user:
-            # ensure hashed password matches user input
+            # ensure password matches user input
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for("profile", username=session["user"]))
             else:
-                # invalid password match
+                # password does not match
                 flash("Incorrect Username and/or Password")
                 return redirect(url_for("login"))
 
         else:
-            # username doesn't exist
+            # username does not exist
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
-
     return render_template("login.html")
 
+
+# User profile page
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
-    # grab the session user's username from db
+    # get session user's name from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-
+    recipes = list(mongo.db.recipes.find(
+        {"added_by": session["user"]}
+    ))
     if session["user"]:
-        return render_template("profile.html", username=username)
+        return render_template(
+            "profile.html", username=username, recipes=recipes)
 
     return redirect(url_for("login"))
 
+
+# User log out
 @app.route("/logout")
 def logout():
-    # remove user from session cookie
+    # remove user from session cookies
     flash("You have been logged out")
     session.pop("user")
     return redirect(url_for("login"))
 
+
+# User add recipe functionality
 @app.route("/add_recipe", methods=["GET", "POST"])
 def add_recipe():
     if request.method == "POST":
+
+        # check image url if one provided by user
+        recipe_image = request.form.get("image_url")
+        image_url = ""
+        if recipe_image:
+            # function call to verify image
+            if is_url_image(recipe_image):
+                image_url = recipe_image
+                flash("Image verified")
+            else:
+                image_url = "../static/images/new-zombie.jpg"
+                flash("Image not valid, will apply default image instead")
+
         recipe = {
             "category_name": request.form.get("category_name"),
-            "recipe_name": request.form.get("recipe_name"),
+            "recipe_name": request.form.get("recipe_name").recipe(),
             "recipe_description": request.form.get("recipe_description"),
+            "image_url": image_url,
             "created_by": session["user"]
         }
         mongo.db.recipes.insert_one(recipe)
-        flash("Recipe Successfully Added")
-        return redirect(url_for("get_recipes"))
+        flash("Your recipe has been successfully added")
+        return redirect(url_for("profile", username=session["user"]))
 
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("add_recipe.html", categories=categories)
 
+
+# User edit recipe functionality
+@app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
+def edit_recipe(recipe_id):
+    if request.method == "POST":
+
+        # check image url if one provided by user
+        recipe_image = request.form.get("image_url")
+        image_url = ""
+        if recipe_image:
+            # function call to verify image
+            if is_url_image(recipe_image):
+                image_url = recipe_image
+                flash("Image verified")
+            else:
+                image_url = "../static/images/new-zombie.jpg"
+                flash("Image not valid, will apply default image instead")
+
+        updated_recipe = {
+            "category_name": request.form.get("category_name"),
+            "recipe_name": request.form.get("recipe_name").recipe(),
+            "recipe_description": request.form.get("recipe_description"),
+            "image_url": image_url,
+            }
+        mongo.db.recipes.update_one(
+            {"_id": ObjectId(recipe_id)}, {"$set": updated_recipe})
+        flash("Your recipe has been successfully updated")
+        return redirect(url_for("profile", username=session["user"]))
+
+    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template(
+        "edit_recipe.html", recipe=recipe, categories=categories)
+
+
+# User delete recipe functionality
+@app.route("/delete_recipe/<recipe_id>")
+def delete_recipe(recipe_id):
+    mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
+    flash("Your recipe has been deleted")
+    return redirect(url_for("profile", username=session["user"]))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
--- static/js/script.js
+++ static/js/script.js
@@ -1,34 +1,50 @@
-$(document).ready(function () {
-    $(".sidenav").sidenav({edge: "right"});
-    $(".collapsible").collapsible();
-    $(".tooltipped").tooltip();
-    $("select").formSelect();
+$(document).ready(function(){
+    $('.sidenav').sidenav({edge: "right"});
+    $('select').formSelect();
+    $('textarea#synopsis, textarea#user_review').characterCounter();
+    $(".dropdown-trigger").dropdown();
+    $('.modal').modal();
 
-    validateMaterializeSelect();
-    function validateMaterializeSelect() {
-        let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
-        let classInvalid = { "border-bottom": "1px solid #f44336", "box-shadow": "0 1px 0 0 #f44336" };
-        if ($("select.validate").prop("required")) {
-            $("select.validate").css({ "display": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
-        }
-        $(".select-wrapper input.select-dropdown").on("focusin", function () {
-            $(this).parent(".select-wrapper").on("change", function () {
-                if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () { })) {
-                    $(this).children("input").css(classValid);
-                }
-            });
-        }).on("click", function () {
-            if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.03)") {
-                $(this).parent(".select-wrapper").children("input").css(classValid);
-            } else {
-                $(".select-wrapper input.select-dropdown").on("focusout", function () {
-                    if ($(this).parent(".select-wrapper").children("select").prop("required")) {
-                        if ($(this).css("border-bottom") != "1px solid rgb(76, 175, 80)") {
-                            $(this).parent(".select-wrapper").children("input").css(classInvalid);
-                        }
-                    }
-                });
-            }
-        });
-    }
+    // code below to fix validation issue with materialize select element taken from Code Institute Mini project 
+  validateMaterializeSelect();
+  function validateMaterializeSelect() {
+      let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
+      let classInvalid = { "border-bottom": "1px solid #f44336", "box-shadow": "0 1px 0 0 #f44336" };
+      if ($("select.validate").prop("required")) {
+          $("select.validate").css({ "display": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
+      }
+      $(".select-wrapper input.select-dropdown").on("focusin", function () {
+          $(this).parent(".select-wrapper").on("change", function () {
+              if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () { })) {
+                  $(this).children("input").css(classValid);
+              }
+          });
+      }).on("click", function () {
+          if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.03)") {
+              $(this).parent(".select-wrapper").children("input").css(classValid);
+          } else {
+              $(".select-wrapper input.select-dropdown").on("focusout", function () {
+                  if ($(this).parent(".select-wrapper").children("select").prop("required")) {
+                      if ($(this).css("border-bottom") != "1px solid rgb(76, 175, 80)") {
+                          $(this).parent(".select-wrapper").children("input").css(classInvalid);
+                      }
+                  }
+              });
+          }
+      });
+  }
 });
+
+// code to hide author column on add review page found on stack overflow https://stackoverflow.com/questions/15566999/how-to-show-form-input-fields-based-on-select-value
+// code to remove required attribute from author name when hidden found on stack overflow https://stackoverflow.com/questions/10407622/addattr-not-working-in-jquery
+$('#category_name').on('change', function(){
+    if ($(this).val() === "Book"){
+        $("#author-col").show();
+        $("#author_name").prop('required', true);
+        } 
+    else {
+        $("#author-col").hide();
+        $("#author_name").prop('required', false);
+    }
+    }
+);
