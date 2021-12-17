55748f7d33f0a8955bbbd79019fbb3ea697ce62d
LukeSmallman <lukesmallman15@gmail.com>
2021-12-17 06:47:07 +0000

    Changing debug to false

--- app.py
+++ app.py
@@ -210,4 +210,4 @@ def forbidden(error):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
